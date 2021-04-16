e8aec6f6124239e30fc9e7641824c26edb2c8610
stromboli1 <54105754+stromboli1@users.noreply.github.com>
2021-04-16 16:22:14 +0200

    Progress

--- Classes.py
+++ Classes.py
@@ -1,42 +1,26 @@
-class chessPiece():
+class pawn():
     def __init__(self,x,y,img):
         self.x = x
         self.y = y
         self.img = img
+        self.firstMove = True
 
-        def move(self,dx,dy):
-            self.dy += dy*125
-            self.dx += dx*125
+class tower():
+    def __init__(self):
+        pass
 
-        def draw(self):
-            pass
+class runner():
+    def __init__(self):
+        pass
 
-        def death(self):
-            pass
+class queen():
+    def __init__(self):
+        pass
 
-class pawn(chessPiece):
-    def __init__(self,x,y,img):
-        self.dx = 0
-        self.dy = 1
-        super().__init__(x,y,img)
+class king():
+    def __init__(self):
+        pass
 
-
-
-class Rectangle:
-    def __init__(self, length, width):
-        self.length = length
-        self.width = width
-
-    def area(self):
-        return self.length * self.width
-
-    def perimeter(self):
-        return 2 * self.length + 2 * self.width
-
-# Here we declare that the Square class inherits from the Rectangle class
-class Square(Rectangle):
-    def __init__(self, length):
-        super().__init__(length, length)
-
-square = Square(4)
-print(square.area())
+class knight():
+    def __init__(self):
+        pass
--- Main.py
+++ Main.py
@@ -1,5 +1,7 @@
 import pygame as pg
+from Classes import pawn
 import os
+import random
 
 pg.init()
 win = pg.display.set_mode((1500,1000))
@@ -7,13 +9,16 @@ pg.display.set_caption("Skak")
 clock = pg.time.Clock()
 path = os.path.abspath(os.getcwd()) + "//Chess_pieces_SVG//"
 
-print(path)
-
-chess_pieces = pg.image.load(path+"b_bishop_png_shadow_1024px.png")
+b_pawn_img = pg.image.load(path+"b_pawn_png_shadow_1024px.png")
 board_brown = pg.image.load(path+"square brown light_png_shadow_1024px.png")
 board_grey = pg.image.load(path+"square gray light _png_shadow_1024px.png")
 
-sc_chess_pieces = pg.transform.scale(chess_pieces,(125,125))
+sc_b_pawn_img = pg.transform.scale(b_pawn_img,(125,125))
+
+blackPieces = []
+
+for i in range(8):
+    blackPieces.append(pawn(250+125*i,125,sc_b_pawn_img))
 
 def redrawGameWindow():
     for x in range(8):
@@ -25,13 +30,21 @@ def redrawGameWindow():
             else:
                 win.blit(pg.transform.scale(board_grey,(125,125)),(250+125*x,125*y))
 
-    win.blit(sc_chess_pieces,(250,250))
+    for i in blackPieces:
+        win.blit(i.img,(i.x,i.y))
 
     pg.display.update()
 
 run = True
+turn = random.randint(0,1)
 while run:
+    pressed1, pressed2, pressed3 = pg.mouse.get_pressed()
+    keys = pg.key.get_pressed()
     for event in pg.event.get():
         if event.type == pg.QUIT:
             run = False
+    if pressed1:
+        pos = pg.mouse.get_pos()
+        print(pos)
+
     redrawGameWindow()
