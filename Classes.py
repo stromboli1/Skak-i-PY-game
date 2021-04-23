3341111c061f19da799629a1aa89365f11d5a675
mreriksen <eriksen2608@gmail.com>
2021-04-23 08:20:34 +0200

    Tilføj variabler til runner, tower + lav promote()

--- Classes.py
+++ Classes.py
@@ -6,15 +6,36 @@ class pawn():
         self.dy = 1
         self.img = img
         self.firstMove = True
-        # if self.firstMove:
+
+        def promote():
+            if self.y == 1 or 8:
+                self.remove()
+                self.add(click())
+                self.x = self.x
+                self.y = self.y
+
+         if self.firstMove:
+             self.dy = 1 or 2
+
+
 
 class tower():
     def __init__(self):
-        pass
+        self.x = x
+        self.y = y
+        self.dx = i or 0
+        self.dy = 0 or i
+        self.img = img
+        self.firstMove = True
 
 class runner():
     def __init__(self):
-        pass
+        self.x = x
+        self.y = y
+        self.dx = i
+        self.dy = i
+        self.img = img
+        self.firstMove = True
 
 class queen():
     def __init__(self):
