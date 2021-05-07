8832a41e39e1eb3be0db49978f65d6d47492f445
Nichlas Overgaard Laugesen <nichlasost@gmail.com>
2021-05-07 09:10:36 +0200

    bugfix (Tilføj brikker)

--- Classes.py
+++ Classes.py
@@ -3,19 +3,10 @@ class pawn():
         self.x = x
         self.y = y
         self.dx = 0
+
         self.img = img
         self.firstMove = True
         self.moves = []
-
-
-    def promote():
-        if self.y == 1 or 8:
-            self.remove()
-            self.add(click())
-            self.x = self.x
-            self.y = self.y
-
-
 
     def moveset(self):
         if self.firstMove:
@@ -24,56 +15,47 @@ class pawn():
             self.moves = [[0,1]]
 
 
-
 class rook():
-    def __init__(self):
+    def __init__(self,x,y,img):
         self.x = x
         self.y = y
         self.dx = 0
+
+        self.img = img
         pass
-
-        if self.firstMove == True:
-            self.dy = 1 or 2
-            self.firstMove == False
-        else:
-            self.dy = 1
-
-        def enPassant():
-            pass
-
 
 class bishop():
     def __init__(self,x,y,img):
         self.x = x
         self.y = y
         self.dx = 0
-        self.dy = 1
+
         self.img = img
-        
+        pass
 
 class queen():
     def __init__(self,x,y,img):
         self.x = x
         self.y = y
         self.dx = 0
-        self.dy = 1
+
         self.img = img
-
-
+        pass
 
 class king():
     def __init__(self,x,y,img):
         self.x = x
         self.y = y
         self.dx = 0
-        self.dy = 1
-        self.img = img
 
+        self.img = img
+        pass
 
 class knight():
     def __init__(self,x,y,img):
         self.x = x
         self.y = y
         self.dx = 0
-        self.dy = 1
+
         self.img = img
+        pass
